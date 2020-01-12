8f0856b313d003e5e714223cf3952eec99a9796b
Chris <chris.791.ix@gmail.com>
2020-01-12 03:12:30 -0800

    front-end showing

--- src/components/ScraperWindow.vue
+++ src/components/ScraperWindow.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app id="app-container">
+    <v-img src="../assets/grad-head.svg" class="grad-head-img" max-height="200" position="top center"></v-img>
     <v-btn class="scrape-btn" @click="scrapeWindow()" dark>START SCRAPING</v-btn>
   </v-app>
 </template>
@@ -11,32 +12,33 @@ export default {
   },
   methods: {
     scrapeWindow() {
+      alert("test");
       // window.location.reload(false);
-      console.log("test");
-      window.onload = function(){
-        const re = new RegExp('cotton', 'gi')
-        const matches = document.documentElement.innerHTML.match(re)
-        // console.log(matches)
-        var images = document.getElementsByTagName('img')
-        var rect = images[10].getBoundingClientRect();
-        // console.log(rect.x)
-        // console.log(rect.y)
-        // console.log(images[0])
-        var box = document.createElement('input')
-        box.style.position = 'absolute'
-        box.style.left = rect.x
-        box.style.top = rect.y
-        if(matches != null){
-            const div = document.createElement('div')
-            div.textContent = 'Eco-score: 60'
-            box.textContent = 'Eco-score: 60'
-        }
-        else{
-            const div = document.createElement('div')
-            div.textContent = 'Eco-score: 100'
-            box.textContent = 'Eco-score: 100'
-        }
-      } 
+      // console.log("test");
+      // window.onload = function(){
+      //   const re = new RegExp('cotton', 'gi')
+      //   const matches = document.documentElement.innerHTML.match(re)
+      //   // console.log(matches)
+      //   var images = document.getElementsByTagName('img')
+      //   var rect = images[10].getBoundingClientRect();
+      //   // console.log(rect.x)
+      //   // console.log(rect.y)
+      //   // console.log(images[0])
+      //   var box = document.createElement('input')
+      //   box.style.position = 'absolute'
+      //   box.style.left = rect.x
+      //   box.style.top = rect.y
+      //   if(matches != null){
+      //       const div = document.createElement('div')
+      //       div.textContent = 'Eco-score: 60'
+      //       box.textContent = 'Eco-score: 60'
+      //   }
+      //   else{
+      //       const div = document.createElement('div')
+      //       div.textContent = 'Eco-score: 100'
+      //       box.textContent = 'Eco-score: 100'
+      //   }
+      // } 
     }
   }
 }
